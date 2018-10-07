104029efbb9be21d361788311cf0d8bd06909360
Alexander Sakharuk <saharuk.alexander@gmail.com>
2018-10-07 15:25:05 +0300

    zbackup changes

--- zbackup.sh
+++ zbackup.sh
@@ -10,11 +10,20 @@
 #
 # DEFAULTS
 #
+
 AUTOREMOVE=10
 LOGFILE="/var/log/zabbix/zbackup.log"
 DBUSER="zabbix"
 DBPASS="zabbix"
+TEMPDIR="/tmp"
 
+TIMESTAMP=$(date +%d-%m-%Y_%H-%M-%S)
+
+#
+# FUNCTIONS
+#
+
+# Function print help message
 function PrintHelp() {
 cat <<EOF
 Zabbix backup script (Version 0.1)
@@ -26,15 +35,26 @@ OPTIONS
 -d <dir>		Path to backup
 -h			Print help
 -l <path>		Path to log file (default: /var/log/zabbix/zbackup.log) 	
+-t <path>		Path to temp directory (default: /tmp) 	
 -u <username> 		MySQL user
 -p <pass>		MySQL password
 
 EOF
 }
 
+# Function remove all temp folders created by this script
+function ClearTemp() {
+	if [[ -d $TEMPDIR ]]; then
+		rm -rf $TEMPDIR/zbackup-* 2>>$OGFILE
+	fi
+}
+
 if [[ $# -eq 0 ]]; then PrintHelp; exit 0; fi 
 
-# Parse options
+#
+# PARSE OPTONS
+#
+
 while getopts "a:d:hl:t:u:p:" opt; do
 	case $opt in
 		a) AUTOREMOVE="${OPTARG}" ;;
@@ -47,12 +67,9 @@ while getopts "a:d:hl:t:u:p:" opt; do
 	esac
 done
 
-# Validate input
-if [[ -z "${BACKUPDIR}" ]]; then echo "ERROR: You must provide backup directory path. Use option -d <dir>." >&2 >>$LOGFILE exit 1; fi
-
 # Create temp folder
-if ! mkdir $TEMPDIR; then echo "ERROR: Can not create temp folder at ${TEMPDIR}" >&2 >>$LOGFILE; exit 1; fi 
-
+if ! mkdir $TEMPDIR; then echo "${TIMESTAMP} ERROR: Can not create temp folder ${TEMPDIR}" >&2 >>$LOGFILE; exit 1; fi 
 # Start backup
 echo "Backup started."
 
+if [[ -z "${BACKUPDIR}" ]]; then echo "ERROR: You must provide backup directory path. Use option -d <dir>." >&2 >>$LOGFILE exit 1; fi
