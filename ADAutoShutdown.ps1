5562def50b6c412c6d9aa8e2dffb230ada7794d8
Alexander Sakharuk <saharuk.alexander@gmail.com>
2019-07-16 15:46:28 +0300

    Powershell auto shutdown script added

---
+++ ADAutoShutdown.ps1
@@ -0,0 +1,42 @@
+# Put this PS script to AD Server scheduler 
+# from 6 to 23 or easily to run it each hour
+#
+# Alexander Sakharuk <saharuk.alexander@gmail.com>
+# https://github.com/caxapyk
+#
+
+$DN = "CN=Computers, DC=arsenal-orel, DC=ru"
+
+for($i=0; $i -le 23; $i++){
+    $date = Get-Date -UFormat '%H'
+    # if current hour equals to group shutdown time do...
+    if ($i -eq $date) {
+        # try to find time groups AutoShutdownAt[i]
+        try
+        {
+            $ComputersInGroup = Get-ADGroupMember -Identity  "CN=AutoShutdownAt$i, $DN" | select name -ExpandProperty name
+            Write-Host "Policy group AutoShutdownAt$i found."
+            # shutdown immediately if computers found in group
+            If ($ComputersInGroup) {
+                $ComputersInGroup | ForEach-Object -Process {
+                    if (Test-Connection -ComputerName $_ -Count 1 -Quiet) {
+                        Write-Host ("    ", $_,  "shutting down...")
+                        try
+                        {
+                            Stop-Computer -ComputerName $_ -Force -ErrorAction Stop
+                        } catch {
+                            Write-Host  ("    ", $_.Exception.Message)
+                        }
+                    } else {
+                        Write-Host ("    ", $_,  "is not avaible!")
+                    }
+                }
+            } else {
+                Write-Host "    Computers in Policy group AutoShutdownAt$i not found."
+            }
+            Remove-Variable ComputersInGroup
+        } catch {
+            Write-Host "Policy group AutoShutdownAt$i not found, skipping..."
+        }
+    }
+}
